a2cca04e9b6e96e4b7a1b77e1443cf4daa42a25b
AmaroPSI <2170400@my.ipleiria.pt>
2018-11-26 16:59:12 +0000

    Tested Cards on Index

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,53 +1,50 @@
 <?php
+use yii\helpers\Html;
 
 /* @var $this yii\web\View */
 
 $this->title = 'My Yii Application';
 ?>
+<style>
+    .card {
+        box-shadow: 0 2px 8px 0 rgba(0,0,0,0.2);
+        transition: 0.3s;
+        width: 400px;
+        height: 300px;
+    }
+
+    .card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
+
+    .container {
+        padding: 2px 16px;
+    }
+</style>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <h1>Os Seus Treinos</h1>
     </div>
 
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+    <div class="body-content" >
+        <div class="card">
+            <?php /* <img  src="Meter Imagem" alt="Avatar" style="width:100%"> */ ?>
+            <div class="container">
+                <h4><b>Treino 1</b></h4>
+                <p>Asd</p>
+                <?= Html::submitButton('Começar Treino', ['class' => 'btn btn-success']) ?>
             </div>
         </div>
-
+    </div>
+    <div class="body-content" >
+        <div class="card">
+            <?php /* <img  src="Meter Imagem" alt="Avatar" style="width:100%"> */ ?>
+            <div class="container">
+                <h4><b>Treino 2</b></h4>
+                <p>Asd</p>
+                <?= Html::submitButton('Começar Treino', ['class' => 'btn btn-success']) ?>
+            </div>
+        </div>
     </div>
 </div>
