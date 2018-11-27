86d9975987cd3e8a867bf6ead1bd2eb88f6f8009
Melicias <melicias1999@gmail.com>
2018-11-27 22:48:48 +0000

    asda
    sdf

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -10,34 +10,60 @@ use yii\widgets\ActiveForm;
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-
     <div class="jumbotron">
         <h1>Os Seus Treinos</h1>
     </div>
 
     <form action="" method="post">
-        <table width="100%" border="0" style="border:none;">
-            <tr>
-                <td><label>Nome: </label><input type="text" name="nome" /></td>
-                <td><label>Dificuldade: </label><select><?php foreach ($dificuldades as $dificuldade){?> <option value="<?=$dificuldade->id_dificuldade?>"><?=$dificuldade->dificuldade?></option><?php } ?></select></td>
-                <td><label>Categoria: </label><input type="text" name="categoria" /></td>
-                <td><input class="button" type="submit" name="submit" value="Search" /></td>
-            </tr>
-        </table>
-        <br><br>
-    </form>
-    <?php for($i = 0;$i<count($treinos);$i++){ ?>
-    <div class="body-content" >
-                <div class="card">
-            <?php /* <img  src="Meter Imagem" alt="Avatar" style="width:100%"> */ ?>
-            <div id="container_index" class="container">
-              <h1> <?=$treinos[$i]->nome?></h1>
-                <h4>Dificuldade: </h4><p><?=$treinos[$i]->dificuldade->dificuldade?></p>
-                <h4>Categoria: </h4><p><?=$treinos[$i]->categoria->nome?></p>
-
-                <?= Html::submitButton('Visualizar Treino', ['class' => 'btn btn-success']) ?>
+        <div class="row">
+            <div class="col-md-3 col-sm-4">
+                <label>Nome: </label>
+                <input class="form-control" type="text" name="nome" />
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <label>Dificuldade: </label>
+                <select class="form-control">
+                    <option value="0">Nenhuma</option>
+                    <?php foreach ($dificuldades as $dificuldade){?>
+                        <option value="<?=$dificuldade->id_dificuldade?>"><?=$dificuldade->dificuldade?></option><?php } ?>
+                </select>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <label>Categoria: </label><select class="form-control">
+                    <option value="0">Nenhuma</option>
+                    <?php foreach ($categorias as $categoria){?>
+                        <option value="<?=$categoria->id_categoria?>"><?=$categoria->nome?></option><?php } ?>
+                </select>
+            </div>
+            <div class="col-sm-6 col-md-1">
+                <br>
+                <input class="btn btn-default btn-primary" type="submit" name="submit" value="Pesquisar" />
             </div>
         </div>
+    </form>
+
+    <br><br>
+    <div class="container-fluid cards-row">
+        <div class="row">
+            <?php for($i = 0;$i<count($treinos);$i++){ ?>
+            <div class="body-content col-sm-6 col-md-4" >
+                <div class="thumbnail">
+                    <div class="caption">
+                        <h1 style="text-align: center"> <?=$treinos[$i]->nome?></h1>
+                        <div style="padding-left: 20px">
+                            <p class="card-description" style="font-size: 16px"><strong style="font-size: 18px">Dificuldade:</strong> <?=$treinos[$i]->dificuldade->dificuldade?></p>
+                            <p class="card-description" style="font-size: 16px"><strong style="font-size: 18px">Categoria:</strong> <?=$treinos[$i]->categoria->nome?></p>
+                            <div style="height: 125px">
+                                <p style="font-size: 12px"><strong style="font-size: 14px">Descrição:</strong> <?=$treinos[$i]->descricao?></p>
+                            </div>
+                        </div>
+                        <div style="text-align: right">
+                            <input class="btn btn-default btn-primary" style="" type="submit" name="submit" value="Visualizar treino" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php };?>
+        </div>
     </div>
-    <?php };?>
 </div>
