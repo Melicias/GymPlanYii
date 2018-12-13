473c1286203e4ae33010428efd92e5688e1d6f66
Amaro-PSI <goncalo.amaro4@hotmail.com>
2018-12-13 16:29:24 +0000

    Functional Admin Login Done

--- backend/tests/functional/LoginAdminCest.php
+++ backend/tests/functional/LoginAdminCest.php
@@ -3,14 +3,46 @@
 namespace backend\tests\functional;
 
 use backend\tests\FunctionalTester;
-use yii\helpers\Url;
 
 class LoginAdminCest
 {
-    public function TryLogin(FunctionalTester $I)
-    {
-        $I->amOnPage(Url::toRoute('/site/login'));
-        $I->see('Login', 'h1');
+    protected $formId = '#login-form';
 
+    public function _before(FunctionalTester $I)
+    {
+        $I->amOnRoute('site/login');
     }
+
+    public function LoginCorrect(FunctionalTester $I)
+    {
+        $I->submitForm($this->formId, [
+            'login-form[email]' => 'melicias1999@gmail.com',
+            'login-form[password]' => 'jayson',
+        ]);
+
+        $I->seeRecord('common\models\Admin', [
+            'primeiroNome' => 'Francisco',
+            'ultimoNome' => 'Melicias',
+            'email' => 'melicias1999@gmail.com',
+        ]);
+
+        $I->see('Login', 'form button[type=submit]');
+    }
+    public function WrongEmailOrPassword(FunctionalTester $I)
+    {
+        $I->submitForm($this->formId, [
+            'login-form[email]' => 'teste@teste.com',
+            'login-form[password]' => 'teste',
+        ]);
+
+        $I->seeRecord('common\models\Admin', [
+            'primeiroNome' => 'asd',
+            'ultimoNome' => 'asd',
+            'email' => 'teste@teste.com',
+
+        ]);
+
+        $I->see('Login', 'form button[type=submit]');
+    }
+
 }
